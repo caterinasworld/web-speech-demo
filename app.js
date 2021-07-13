a5a8bdf4dc370dce58b086a22835bf8bcb130fc5
Caterina Paun <caterina.paun@gmail.com>
2021-07-13 09:54:12 -0700

    Add starter files for web speech api demo

---
+++ app.js
@@ -0,0 +1,56 @@
+// Create new instance - speech synthesis
+const synthesis = window.speechSynthesis;
+
+// Establish that browser has speech recognition
+window.SpeechRecognition =
+  window.SpeechRecognition || window.webkitSpeechRecognition;
+
+// Create new instance - speech recognition
+const recognition = new SpeechRecognition();
+// recognition.interimResults = true;
+recognition.continuous = true;
+recognition.lang = 'en-US';
+
+// Grab elements from the DOM
+let startRecognition = document.querySelector('#start');
+let stopRecognition = document.querySelector('#stop');
+let clearTranscript = document.querySelector('#clear');
+let playSpeech = document.querySelector('#play');
+let transcript = document.querySelector('#transcript');
+
+// Add event listeners
+startRecognition.addEventListener('click', handleStartRecognition);
+stopRecognition.addEventListener('click', handleStopRecognition);
+clearTranscript.addEventListener('click', handleClearTranscript);
+playSpeech.addEventListener('click', handlePlaySpeech);
+
+function handleStartRecognition(event) {
+  console.log('start speech recognition');
+
+  recognition.addEventListener('result', (event) => {
+    const results = Array.from(event.results)
+      .map((result) => result[0].transcript)
+      .join('');
+
+    console.log(results);
+    transcript.innerHTML = results;
+  });
+
+  recognition.start();
+}
+
+function handleStopRecognition(event) {
+  console.log('stop speech recognition');
+  recognition.stop();
+}
+
+function handleClearTranscript(event) {
+  transcript.innerHTML = '';
+}
+
+function handlePlaySpeech(event) {
+  console.log('speech synthesis');
+
+  let utterance = new SpeechSynthesisUtterance(transcript.innerHTML);
+  synthesis.speak(utterance);
+}
